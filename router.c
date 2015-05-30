f9ea6dbe8f3c44e4dacba73b70342671b1cc4ff2
cs217b <peter.t.bankole@gmail.com>
2015-05-30 08:10:33 -0700

    Cleaned up code

--- router.c
+++ router.c
@@ -1,3 +1,9 @@
+
+/*Project 2 
+
+
+*/ 
+
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <stdio.h> 
@@ -28,50 +34,30 @@
 
 #include <pthread.h> 
 
-#define NUM_THREADS 1 
-#define BUFFER_SIZE 1024 
-#define MAX_NODE 1024 
-
-
-#define DV_FILE_OPEN_FAILED -1000 
-
+#include <arpa/inet.h>
 
 using namespace std; 
 
-pthread_mutex_t mutexRecv;
-//Get Neighbors from file and NodeName 
+#define BUFSIZE 2048
 
+#define DV_FILE_OPEN_FAILED -1000
+
+pthread_mutex_t mutexRecv; 
 struct neighbors { 
 
 	char name;	
 	int port; 
 	int cost; 
+	char ipAddress[48]; 
 
 }; 
 vector<struct neighbors> n; 
 
 typedef struct {
 	int sockfd; 
-	std::string remoteServer;  
-	char nodeName; 
-	
-}sendArg; 
-
-typedef struct {
-	int sockfd; 
 	char nodeName; 
 	
 }recvArg; 
-
-
-struct forwardingTable { 
-	char dest; 
-	int cost; 
-	char nextHop; 
-}; 
-forwardingTable ft; 
-
-vector<struct forwardingTable> table; 
 
 struct routingTable { 
 	char src; 
@@ -82,93 +68,6 @@ struct routingTable {
 }; 
 vector<struct routingTable> routeList;  
 
-void _setupTimer(int seconds) {
-	sleep(seconds); 
-}
-
-
-void _udpOpenSocket(int &fd ) {
-
-	//Create a Socket 
-	//_param 
-	// AF_INET: domain/IPv4
-	//SOCK_DRAM: type of service/UDP
-	//0  PROTOCOL 
-	if ((fd = socket(AF_INET, SOCK_DGRAM,0)) < 0 ) {
-		perror ("Error creating socket!!");  
-		exit(1); 
-	}
-}
-
-void _udpServerSetup(int &fd, int portno) {
-
-	struct sockaddr_in serv_addr; 
-	//bind to arbituary address
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-     	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
-
-	if (bind(fd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              perror("ERROR on binding");
-}
-
-
-int  _udpRecv(int &fd, char *msg) {
-
-	int recvLen;  
-	struct sockaddr_in remaddr;
-	socklen_t len = sizeof(remaddr);  
-
-
-	//int recvfrom(int socket, void *restrict buffer,
-	// size_t length, int flags, struct sockaddr *restrict src_addr,
-        // socklen_t *restrict *src_len)
-	recvLen = recvfrom(fd,msg, BUFFER_SIZE,MSG_DONTWAIT,(struct sockaddr *)\
-&remaddr,&len);   
-
-
-	return recvLen;  
-}
-
-void _udpSend(int &fd, char *msg,std::string remoteServerHostName, int remoteServerPort) {
-	struct hostent *hp;
-        int i;
-	struct sockaddr_in remaddr;  
-	//Wait for 1000 milliseconds 
-	//_setupTimer();   
-
-	char *host;  
-	host = new char[remoteServerHostName.length() + 1];  
-	strcpy(host, remoteServerHostName.c_str()); 
-	
-	hp =gethostbyname(host);  	
-
-	if (!hp) {
-		perror("Could not obtain host  address"); 
-		exit(1);  
-        }
-
-       
-	memset((char*)&remaddr, 0, sizeof(remaddr));
-	remaddr.sin_family = AF_INET;
-	remaddr.sin_port = htons(remoteServerPort);
-
-	/* put the host's address into the server address structure */
-	memcpy((void *)&remaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
-
-
-	/* send a message to the server */
-	if (sendto(fd, msg,strlen(msg) , 0, (struct sockaddr *)&remaddr, sizeof(remaddr)) < 0) {
-	perror("sendto failed");
-		exit(1);  
-	} 
-
-
-
-
-} 
 
 int  getRoutesFromFile(vector<struct neighbors> &n, char &nodeName) {
 
@@ -237,6 +136,7 @@ int  getRoutesFromFile(vector<struct neighbors> &n, char &nodeName) {
 				//Cost to Neighbor 
 				char tmpChar = str[str.length() - 2];  
 				neighbor.cost = abs(tmpChar - '0'); 
+
 				
 				//save neighbor 
 				n.push_back(neighbor); 
@@ -261,7 +161,15 @@ int  getRoutesFromFile(vector<struct neighbors> &n, char &nodeName) {
 					//Cost to Neighbor 
 					char tmpChar = str[str.length() - 2];  
 					neighbor.cost = abs(tmpChar - '0'); 
-	
+
+
+					//Generate an Ip address for neighbor a
+					std::string ipstr; 
+					ipstr = "127.0.0.1"; 
+					strcpy(neighbor.ipAddress,ipstr.c_str()); 
+					sizeof(neighbor.ipAddress); 
+					neighbor.ipAddress[sizeof(neighbor.ipAddress) - 1] = 0; 
+
 					//save neighbor 
 					n.push_back(neighbor); 
 			
@@ -327,105 +235,12 @@ int getNodePortNumber(char nodeName) {
 
 }
 
-
-static void *TimerRoutine(void *args) {
-
-	//send ALOHA to neighbors every 5 seconds 
-	//For simplicity the payload/header is also included 
-	//in the packet. The packet is the routing table 
-
-	//PACKET STRUCTURE : 
-	/*
-	ALOHA#CNTL#NODESID#NODESPORT#NEIGHBORID#NEIGHBORPORT#COSTTNEIGHBOR. 
-			OR 
-	ALOHA#DATA#NODESID#NODESPORT#NEIGHBORID#NEIGHBORPORT#COSTTNEIGHBOR. 
-	*/
-
-	sendArg arg = *((sendArg *)args); 
-
-	char msg[BUFFER_SIZE];  
-
-	std::string controlData; 
-	//Route List 
-	routingTable rt; 
-	for (int i = 0; i < n.size(); i++) {
-		rt.dest =  n.at(i).name; 
-		rt.destPort = n.at(i).port; 
-		rt.destCost = n.at(i).cost; 
-		routeList.push_back(rt); 
-
-	}
-
-	while(1) {
-		std::stringstream ss; 
-	 	char src = arg.nodeName;
-		int srcPort =getNodePortNumber(src);
-		
-			char dest;
-		int destPort; 
-		int destCost;
-
-		for (int k = 0; k < n.size(); k++) { 
-
-			for (int i = 0; i < routeList.size(); i++) {
-				
-					
-				dest = routeList.at(i).dest;
-				destPort = routeList.at(i).destPort;
-				destCost = routeList.at(i).destCost; 
-				cout <<dest <<" "; 	
-				ss << "ALOHA#" << "CNTL#" << src << "#" << srcPort << "#"; 
-				//copy data into buffer 
-				ss << dest << "#" <<destPort <<"#" << destCost <<".";  
-
-				ss >> controlData;  
-	
-				for (int j = 0 ; j < controlData.length(); j++ ) {
-
-					msg[j] = controlData[j];  
-				} 
-			
-
-				_udpSend(arg.sockfd,msg,arg.remoteServer,n.at(k).port) ; 
-
-				ss.str("");  
-				controlData = ""; 
-
-			} 
-		
-		}
-		_setupTimer(1); 
-		cout <<endl; 
-	} 
-
-
-} 
-
-//Used to detect Control C 
-void my_handler(int s){
-           printf("Caught signal %d\n",s);
-           exit(1); 
-
-}
-
-void *controlC(void *threadId) { 
-	//Used to detect if Ctrl-C is pressed 
-	struct sigaction sigIntHandler;
-	sigIntHandler.sa_handler = my_handler;
-	sigemptyset(&sigIntHandler.sa_mask);
-	sigIntHandler.sa_flags = 0;
-	sigaction(SIGINT, &sigIntHandler, NULL); 
-
-
-}
 void updateRoute(char recvMessage[], char nodeName) {
 
 
 	//Extract The message 
 	std::string msg(recvMessage); 
-//	cout <<msg <<endl; 
 	routingTable rt; 
-
 	std::string tmp; 
 	if (msg.substr(6,4) == "CNTL") {
 		//Extract Header 
@@ -448,12 +263,13 @@ void updateRoute(char recvMessage[], char nodeName) {
 		bool found = false; 
 		int k = 0; 	
 		for (int i = 0; i < routeList.size(); i++ ) {
-			
+
 			if (rt.destPort == routeList.at(i).destPort) {
 				found = true; 
 				k = i; 
 				break; 
-			}
+			} 
+
 
 		}
 		if (found == false) {
@@ -461,42 +277,9 @@ void updateRoute(char recvMessage[], char nodeName) {
 			if (rt.dest != nodeName) {
 				routeList.push_back(rt);  
 			}
-		} else { 
-
-			bool check = false; 
-			for (int i = 0; i < routeList.size(); i++ ) {
-				
-				if (routeList.at(i).destPort == rt.srcPort) {
-
-					int getCost = routeList.at(i).destCost; 
-					int sum ; 
-					if (nodeName == routeList.at(i).dest) {
-						sum = rt.destCost; 
-					} else { 
-					    sum = getCost + rt.destCost;  
-					}
-
-					if (sum < routeList.at(k).destCost) {
-						rt.destCost = sum; 
-						routeList.erase(routeList.begin() + k); 
-						routeList.push_back(rt); 
-
-					}
-						
-					break; 		
-					check = true; 
-				}
-
-			} 
-			if (check == false) {
-
-				//Route most not exist, remove routes with rt.srcPort in routeList...
-			}	
-			
-
 		} 
-
 	}
+
 
 	for (int i = 0; i < routeList.size(); i++) {
 		
@@ -504,120 +287,176 @@ void updateRoute(char recvMessage[], char nodeName) {
 	} 	
 	cout <<endl; 
 
-
-	// check the forwarind table if it has the neighbors of this node
-	// if there is neighbors --- good, if not , 
-	// then remove dea.- check .route.data - remove the .
-
-
 }
-
-
 void *recvData(void *args) { 
 	
-	char recvMessage[BUFFER_SIZE];  
+	char recvMessage[BUFSIZE];  
 	
 	recvArg arg = *((recvArg *)args); 
+	int recvlen;		/* # bytes in acknowledgement message */
+	struct sockaddr_in remaddr;
+	socklen_t addrlen = sizeof(remaddr);  
 
 	//int sockfd = (int)socket; 
-	while(1) { 
-		_udpRecv(arg.sockfd, recvMessage); 
+	while(1) {  
 
-		if (recvMessage[0] == 'A' &&
+
+		recvlen = recvfrom(arg.sockfd, recvMessage, BUFSIZE, MSG_DONTWAIT, (struct sockaddr *)&remaddr, &addrlen);
+
+		cout << "Received Result: " <<recvlen << " "<< string(recvMessage)<<endl; 
+
+		if (recvlen > 0) {
+			recvMessage[recvlen] = 0;
+			if (recvMessage[0] == 'A' &&
 			recvMessage[1] == 'L' &&
 			recvMessage[2] == 'O' &&
 			recvMessage[3] == 'H' && 
 			recvMessage[4] == 'A') {
 
-			pthread_mutex_lock(&mutexRecv);  
-			updateRoute(recvMessage, arg.nodeName);
-			pthread_mutex_unlock(&mutexRecv);  
+				pthread_mutex_lock(&mutexRecv);  
+				updateRoute(recvMessage, arg.nodeName);
+				pthread_mutex_unlock(&mutexRecv);  
 
-			memset(recvMessage,0,strlen(recvMessage));  
 
-		}  
+				memset(recvMessage,0,strlen(recvMessage));  
 
-		_setupTimer(1); 
+			} 
 
+
+		}
+
+
+		sleep(1); 
 	
 	}
 
 } 
 
 
-void run(void) {
 
-	int sockfd = -1; 
-	struct sockaddr_in remaddr; 
-	char rcvMsg[BUFFER_SIZE];  
+int main (int arc, char **argv) {
+
+	struct sockaddr_in myaddr;
+	int fd, i;
+	char buf[BUFSIZE];	/* message buffer */
+	char server[48] ;	/* change this to use a different server */
+
+	/* create a socket */
+
+	if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+		printf("socket created\n");
+
+	memset((char *)&myaddr, 0, sizeof(myaddr));
+	myaddr.sin_family = AF_INET;
 
 	char nodeName; 
-	 getRoutesFromFile(n,nodeName); 
-
-
-	 _udpOpenSocket(sockfd);  
-
+	getRoutesFromFile(n,nodeName); 
 	int serverPortNumber = getNodePortNumber(nodeName); 
-	_udpServerSetup(sockfd,serverPortNumber);
+	
+	std::string ipstr; 
+	ipstr = "127.0.0.1"; 
+	strcpy(server,ipstr.c_str()); 
+	sizeof(server); 
+	server[sizeof(server) - 1] = 0; 
+
+	/* bind it to all local addresses and pick any port number */
+	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	myaddr.sin_port = htons(serverPortNumber);
+
+	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+		perror("bind failed");
+		return 0;
+	}       
+
+	struct sockaddr_in remaddr[n.size()]; 
+	struct sockaddr_in dummy; 
+	int slen=sizeof(dummy); 
+	
+	routingTable rt; 
+	for (int i = 0; i < n.size(); i++ ) {
+		/* now define remaddr, the address to whom we want to send messages */
+		/* For convenience, the host address is expressed as a numeric IP address */
+		/* that we will convert to a binary format via inet_aton */
+
+		memset((char *) &remaddr[i], 0, sizeof(remaddr[i]));
+		remaddr[i].sin_family = AF_INET;
+		long port = n.at(i).port; 
+		remaddr[i].sin_port = htons(port);
+		if (inet_aton(server, &remaddr[i].sin_addr)==0) {
+			fprintf(stderr, "inet_aton() failed\n");
+			exit(1);
+			}
+
+		//Also initialize routing table with neighbors info
+		rt.dest =  n.at(i).name; 
+		rt.destPort = n.at(i).port; 
+		rt.destCost = n.at(i).cost; 
+		routeList.push_back(rt); 
 
 
+	} 
 
-	char recvMessage[5];  
-	std::string remoteServer = "127.0.0.1";  
-
+	//mutex
 	pthread_mutex_init(&mutexRecv, NULL);  
 
-	//Send Thread
-	pthread_t sendThread ; 
+	//Thread attr
 	pthread_attr_t attr; 
 	pthread_attr_init(&attr);  
-	//Argument 
-	sendArg *args = new sendArg;  
-	args->sockfd = sockfd; 
-	args->remoteServer = remoteServer;  
-	args->nodeName = nodeName; 
-	//Periodically send ALOHA and Routing Updates to 
-	//neighbors
-	pthread_create(&sendThread, &attr, TimerRoutine,args);  
 
-	//Receive Thread 	
+	//Receive Thread
 	pthread_t recvThread ; 
 	recvArg *rArgs = new recvArg; 
-	rArgs->sockfd = sockfd; 
+	rArgs->sockfd = fd; 
 	rArgs->nodeName = nodeName; 
 	pthread_create(&recvThread, &attr, recvData,rArgs);  
 
 
-	//Ctr-C Trhread
-	pthread_t controlCThread ; 
-	pthread_attr_t cntAttr; 
-	pthread_attr_init(&cntAttr); 
-	int i = 0; 
-	pthread_create(&controlCThread, &cntAttr, controlC,(void*)i);  
+	char dest;
+	int destPort; 
+	int destCost;
 
+	char src = nodeName; 
+	int srcPort = serverPortNumber;
+	
+	std::string controlData;
+	std::stringstream ss; 
+	while (1) {
+		
+		for (int i = 0; i < n.size(); i++ ) {
 
-	while(1) {;} 
+			for (int j = 0; j < routeList.size(); j++) {
 
+				dest = routeList.at(i).dest;
+				destPort = routeList.at(i).destPort;
+				destCost = routeList.at(i).destCost; 
+				ss << "ALOHA#" << "CNTL#" << src << "#" << srcPort << "#"; 
+				//copy data into buffer 
+				ss << dest << "#" <<destPort <<"#" << destCost <<".";  
+				ss >> controlData;
+		
+				for (int k = 0 ; k < controlData.length(); k++ ) {
+
+						buf[k] = controlData[k];  
+				} 
+
+				if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr[i], slen)==-1) {
+					perror("sendto");
+					exit(1);
+				}
+				ss.str(""); 
+				controlData = ""; 
+			
+			}
+		}
+
+		sleep(1); 
+
+			
+	} 
+	
 	pthread_mutex_destroy(&mutexRecv); 
-	pthread_cancel(sendThread); 
-	pthread_join(sendThread,NULL); 
 	pthread_cancel(recvThread); 
 	pthread_join(recvThread,NULL); 
-	pthread_cancel(controlCThread); 
-	pthread_join(controlCThread,NULL); 
 
-
+	return 0; 
 }
-
-
-
-
-int main(int argc, char *argv[]) {
-	
-
-	run();  
-
-	return 0;  
-}
-
-
